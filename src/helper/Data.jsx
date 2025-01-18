29a633e25786443ee4698edeaea0226753bf5387
Wise <wisewebworks@outlook.com>
2025-01-18 22:52:26 +0100

    Initial setup for React Hospital Appointment app

---
+++ src/helper/Data.jsx
@@ -0,0 +1,71 @@
+export const hastaData = [
+  {
+    id: 1,
+    patientName: 'John Smith',
+    day: 'Dec 12th at 2:30pm',
+    isDone: false,
+    myDoctor: 'Dr. Lisa Cuddy',
+  },
+  {
+    id: 2,
+    patientName: 'Emily Davis',
+    day: 'Dec 13th at 1:30pm',
+    isDone: true,
+    myDoctor: 'Dr. Jessica Adams',
+  },
+  {
+    id: 3,
+    patientName: 'Michael Johnson',
+    day: 'Dec 14th at 3:00pm',
+    isDone: false,
+    myDoctor: 'Dr. Gregory House',
+  },
+  {
+    id: 4,
+    patientName: 'Sophia Brown',
+    day: 'Dec 12th at 2:30pm',
+    isDone: false,
+    myDoctor: 'Dr. James Wilson',
+  },
+  {
+    id: 5,
+    patientName: 'David Miller',
+    day: 'Dec 13th at 1:30pm',
+    isDone: true,
+    myDoctor: 'Dr. Lisa Cuddy',
+  },
+  {
+    id: 6,
+    patientName: 'Olivia Taylor',
+    day: 'Dec 14th at 3:00pm',
+    isDone: false,
+    myDoctor: 'Dr. Jessica Adams',
+  },
+];
+
+export const doctorData = [
+  {
+    id: 1,
+    doctorName: 'Dr. Lisa Cuddy',
+    doctorImg:
+      'https://external-content.duckduckgo.com/iu/?u=https://img.freepik.com/free-photo/beautiful-young-female-doctor-looking-camera-office_1301-7807.jpg',
+  },
+  {
+    id: 2,
+    doctorName: 'Dr. Jessica Adams',
+    doctorImg:
+      'https://external-content.duckduckgo.com/iu/?u=https://img.freepik.com/free-photo/portrait-beautiful-female-doctor_329181-1420.jpg',
+  },
+  {
+    id: 3,
+    doctorName: 'Dr. Gregory House',
+    doctorImg:
+      'https://external-content.duckduckgo.com/iu/?u=https://img.freepik.com/free-photo/smiling-doctor-with-strethoscope-isolated-grey_651396-974.jpg',
+  },
+  {
+    id: 4,
+    doctorName: 'Dr. James Wilson',
+    doctorImg:
+      'https://external-content.duckduckgo.com/iu/?u=https://img.freepik.com/free-photo/doctor-smiling-with-stethoscope_1154-36.jpg',
+  },
+];
